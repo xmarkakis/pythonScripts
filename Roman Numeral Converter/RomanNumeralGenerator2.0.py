0f72758fb72171679bbfd6819760d7b570e57af4
xmarkakis <christos.markakis@icloud.com>
2018-12-19 12:38:44 -0500

    Unit test file and update to 2.0 file

--- Roman Numeral Converter/RomanNumeralGenerator2.0.py
+++ Roman Numeral Converter/RomanNumeralGenerator2.0.py
@@ -5,8 +5,8 @@ This is a script to convert numbers into roman numeral. It really only supports 
 Reason being, roman numerals have weird characters for 5,000 and above. Since I hanve't found a way to insert those special characters,
 I just added the lower case values of those characters in the mean time.
 """
-romanNum = {"a":"I","b":"V","c":"X","d":"L","e":"C","f":"D","g":"M","h":"v","i":"x"}
-romanNum2 ={"1a":"I","1b":"V","1c":"X","2a":"X","2b":"L","2c":"C","3a":"C","3b":"D","3c":"M","4a":"M","4b":"v","4c":"x"}
+romanNum = {"0x":"Converted Roman Numeral = ","0y":"You entered 0 .... 0 is 0 in roman numerals .... come on!!","4a":"I","4b":"V","4c":"X","3a":"X","3b":"L","3c":"C","2a":"C","2b":"D","2c":"M","1a":"M","1b":"v","1c":"x"}
+#romanNum2 ={"0x":"Converted Roman Numeral = ","1a":"I","1b":"V","1c":"X","2a":"X","2b":"L","2c":"C","3a":"C","3b":"D","3c":"M","4a":"M","4b":"v","4c":"x"}
 """
 This is a temp version of the final script. I've overhauling all the code from the original. 
 I'm determined to get this script down to a simple few lines. Here are the steps I think the script needs to do:
@@ -27,22 +27,27 @@ def negCheck(x):
         return checkLen(m)
 
 def checkLen(x):
+    chris = int(x)
+    anthoula = chris + 10000
+    x = list(anthoula)
+    #x=int(x)+10000
+    #x=list(x)
     #This function essentially checks the length of the number and then returns the appropriate order of roman numberal function calls based on the length. 
-    if len(x)==4:
+    if len(x)==5:
         #figure out a for loop here to iterate through the string and create the value variable and the index variable. 
-        return roman(x[0]) + roman(x[1]) + roman(x[2]) + roman(x[3])   
-    elif len(x)==3:
-        print(x)
-        return romanIII(x[0]) + romanII(x[1]) + romanI(x[2])
-    elif len(x)==2:
-        print(x)
-        return romanII(x[0]) + romanI(x[1])         
-    elif len(x)==1:
-        if x == 0: 
-            print("You entered 0 .... 0 is 0 in roman numerals .... come on!!")
-        else:
-            return romanI(x)  
-    elif len(x)>4:
+        return romanNum["0x"] + roman(x[1,1]) + roman(x[2],2) + roman(x[3],3)   
+#    elif len(x)==3:
+#        print(x)
+#        return romanNum2["0x"] + roman(x[1,1]) + roman(x[2],2) + roman(x[3],3)  
+#    elif len(x)==2:
+#        print(x)
+#        return romanNum2["0x"] + roman(x[1,1]) + roman(x[2],2) + roman(x[3],3)       
+#    elif len(x)==1:
+#        if x == 0: 
+##            print("You entered 0 .... 0 is 0 in roman numerals .... come on!!")
+#        else:
+#            return romanI(x)  
+    elif len(x)>10000:
         print("Nice Try ;-) Please print a value in range")
     else:
         print("Please print a value in range")
@@ -53,32 +58,39 @@ def roman(x,y):
     #evaluates when the number is 5 or 0 and then returns the appropriate character. Same rule in all the other roman functions.
     if a==0 or a==5:
         if a==5:
-            r = x + y
-            p = romanNum2[r]
+            r = y+"b"
+            p = romanNum[r]
             return p
         else:
             return ""
     #evaluates when the number is 4 or 9 and then returns the appropriate character. 
     elif a==4 or a==9:
         if a==4:
-            x=romanNum["a"]
-            y=romanNum["b"]
-            p=x+y
+            r = y+"a"
+            s = y+"b"
+            t=romanNum[r]
+            u=romanNum[s]
+            p=t+u
             return p
         else:
-            x=romanNum["a"]
-            y=romanNum["c"]
-            p=x+y
+            r = y+"a"
+            s = y+"c"
+            t=romanNum[r]
+            u=romanNum[s]
+            p=t+u
             return p
     #evaluates when the number is between 1-3 or 6-8 and then returns the appropriate character. 
     elif a>0 and a<9:
         if a<4:
-            p=a*romanNum["a"]
+            r = y+"a"
+            p=a*romanNum[r]
             return p
         else:
-            x=(a-5)*romanNum["a"]
-            y=romanNum["b"]
-            p=y+x
+            r=y+"a"
+            s=y+"b"
+            t=(a-5)*romanNum[r]
+            u=romanNum[s]
+            p=t+u
             return p
     #General error handling code
     else:
@@ -86,12 +98,3 @@ def roman(x,y):
         #case 2 - When the value is zero, need to return the function somehow to the next function
 #remember to convert values to int and abs so that you change the string into a value and also get the absolute value as well
 print(negCheck(numX))
-
-def var(c):
-    for n in var:
-        if index position == 1
-        return a + n
-        else: 
-#Create a for loop that evaluates the index as well as the number and returns the dictionary value for the index and value pair. Maybe a while loop as well?
-#Treat the "1" in "10,000" as a key intro for the response? So something like "Roman Numeral = "??
-
